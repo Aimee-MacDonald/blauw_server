64c094b3a110ddd4d08801d589ca09a5e3df0680
Aimée <aimeelmacdonald@gmail.com>
2021-05-20 16:32:38 +0200

    Store bookings in database

---
+++ dbmodels/Booking.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+  _id: {type: String, required: true},
+  booking_name: {type: String, required: true},
+  checkin_date: {type: Number, required: true},
+  room: {type: Number, required: true},
+  nights: {type: Number, required: true}
+})
+
+module.exports = mongoose.model('booking', schema)
--- server.js
+++ server.js
@@ -2,13 +2,28 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const path = require('path')
+const mongoose = require('mongoose')
 
 const {createBooking} = require(path.join(__dirname, 'state_management/actions/bookings'))
+const Booking = require(path.join(__dirname, 'dbmodels/Booking.js'))
+
+mongoose.connect('mongodb://localhost/blauw', {useNewUrlParser: true, useUnifiedTopology: true});
 
 io.on('connection', socket => {
-  socket.on('bookings', action => {
-    console.log('Something about bookings?')
-    console.log(action)
+  socket.on('bookings', ({type, payload}) => {
+    switch(type){
+      case 'CREATE_BOOKING':
+        const newBooking = new Booking({
+          _id: payload.id,
+          booking_name: payload.name,
+          checkin_date: payload.date,
+          room: payload.room,
+          nights: payload.nights
+        })
+
+        newBooking.save(error => {if(error) console.log(error)})
+    }
+
     /*
     socket.emit('dispatchAction', createBooking({
       name: 'Charlene',
