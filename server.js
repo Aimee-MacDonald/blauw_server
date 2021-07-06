e267f5886fea7bdf7818615cfb632fed9f163f5a
Aimée <aimeelmacdonald@gmail.com>
2021-07-06 12:52:30 +0200

    Server refactoring

--- server.js
+++ server.js
@@ -1,110 +1,15 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-
 const server = http.createServer(app)
-const io = require('socket.io')(server)
+const path = require('path')
+const mongoose = require('mongoose')
 
+mongoose.connect('mongodb://localhost/blauw', {useNewUrlParser: true, useUnifiedTopology: true});
+require(path.join(__dirname, 'socket_management/SocketManager'))(server)
 server.listen(8080, () => console.log('Server listening on Port: 8080'))
 
 app.get('/', (req, res) => {
   console.log('Home Connection')
   res.send('<h1>Home</h1>')
-})
-
-const path = require('path')
-const mongoose = require('mongoose')
-
-const {createBooking} = require(path.join(__dirname, 'state_management/actions/bookings'))
-const {createRoom} = require(path.join(__dirname, 'state_management/actions/rooms'))
-
-const Booking = require(path.join(__dirname, 'dbmodels/Booking.js'))
-const Room = require(path.join(__dirname, 'dbmodels/Room.js'))
-
-mongoose.connect('mongodb://localhost/blauw', {useNewUrlParser: true, useUnifiedTopology: true});
-
-io.on('connection', socket => {
-  socket.on('bookings', ({type, payload}) => {
-    switch(type){
-      case 'CREATE_BOOKING':
-        const newBooking = new Booking({
-          _id: payload._id,
-          booking_name: payload.booking_name,
-          checkin_date: payload.checkin_date,
-          room: payload.room,
-          nights: payload.nights,
-          status: payload.status,
-          checked_in: payload.checked_in
-        })
-
-        newBooking.save(error => {if(error) console.log(error)})
-        break
-
-      case 'DELETE_BOOKING':
-        Booking.deleteOne({_id: payload}, error => {if(error) console.log(error)})
-        break
-
-      case 'UPDATE_BOOKING':
-        Booking.updateOne({_id: payload._id}, {...payload}, error => {if(error) console.log(error)})
-        break
-
-      case 'REFRESH_BOOKINGS':
-        const bookings = Booking.find({}, (error, result) => {
-          if(error){
-            console.log(error)
-          } else {
-            result.forEach(booking => socket.emit('dispatchAction', createBooking(booking)))
-          }
-        })
-        break
-
-      case 'CHECKIN_BOOKING':
-        Booking.updateOne({_id: payload}, {
-          status: 'checked_in',
-          checked_in: true
-        }, error => {if(error) console.log(error)})
-        break
-
-      case 'CHECKOUT_BOOKING':
-        Booking.updateOne({_id: payload}, {
-          status: 'checked_out',
-          checked_in: false
-        }, error => {if(error) console.log(error)})
-        break
-
-      case 'UNDO_CHECKOUT':
-        Booking.updateOne({_id: payload}, {
-          status: 'checked_in',
-          checked_in: true
-        }, error => {if(error) console.log(error)})
-        break
-    }
-  })
-
-  socket.on('rooms', ({type, payload}) => {
-    switch(type){
-      case 'CREATE_ROOM':
-        const newRoom = new Room(payload)
-        newRoom.save(error => {if(error) console.log(error)})
-        break
-      
-      case 'DELETE_ROOM':
-        console.log('Delete Room')
-        console.log(payload)
-        break
-
-      case 'UPDATE_ROOM':
-        console.log('Update Room')
-        console.log(payload)
-
-      case 'REFRESH_ROOMS':
-        Room.find({}, (error, result) => {
-          if(error){
-            console.log(error)
-          } else {
-            result.forEach(room => socket.emit('dispatchAction', createRoom(room)))
-          }
-        })
-    }
-  })
 })
